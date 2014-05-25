9b2aed19f2f8adf62166b7d35e9489d7d96602ba
taniki <tk@deveha.com>
2014-05-25 20:13:41 +0200

    shutdown test on hold api architecture

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,10 +3,10 @@ from wekeypedia.importer.wkp_api import WeKeyApi as api
 from wekeypedia.wikipedia_page import url2title
 from wekeypedia.wikipedia_page import url2lang
 
-def test_api_pages():
-	a = api()
+# def test_api_pages():
+# 	a = api()
 
-	assert len(a.get_pages()) > 0
+# 	assert len(a.get_pages()) > 0
 
 def test_fn_url2title():
 	url = "http://en.wikipedia.org/wiki/Ant"
