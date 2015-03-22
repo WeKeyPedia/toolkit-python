29a267179117ec9dc0b4046087c4d1e4dd09e611
taniki <tk@deveha.com>
2015-03-22 18:25:59 +0100

    add necessary file for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'wekeypedia',
+  packages = ['wekeypedia'], # this must be the same as the name above
+  version = '0.1',
+  description = 'toolkit to build datasets around wikipedia pages and to compute extra metrics',
+  author = 'tam kien duong',
+  author_email = 'tk@deveha..com',
+  url = 'https://github.com/wekeypedia/toolkit-pyton', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['wikipedia', 'information retrieval', 'api', "data"], # arbitrary keywords
+  classifiers = [],
+)
