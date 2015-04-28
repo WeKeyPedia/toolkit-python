38c79844b5598173cdece35a180cf8b50995ff3c
taniki <tk@deveha.com>
2015-04-28 14:50:43 +0200

    update version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
   name = 'wekeypedia',
 #  packages = ['wekeypedia'], # this must be the same as the name above
   packages = find_packages(exclude=["test*"]), # this must be the same as the name above
-  version = '0.1.4',
+  version = '0.1.5',
   description = 'toolkit to build datasets around wikipedia pages and to compute extra metrics',
   author = 'tam kien duong',
   author_email = 'tk@deveha.com',
   url = 'https://github.com/wekeypedia/toolkit-python', # use the URL to the github repo
-  download_url = 'https://github.com/WeKeyPedia/toolkit-python/archive/0.1.4.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/WeKeyPedia/toolkit-python/archive/0.1.5.tar.gz', # I'll explain this in a second
   keywords = ['wikipedia', 'information retrieval', 'api', "data"], # arbitrary keywords
   classifiers = [],
   install_requires=["nltk", "networkx", "requests", "mwparserfromhell", "colorama", "beautifulsoup4", "numpy"],
